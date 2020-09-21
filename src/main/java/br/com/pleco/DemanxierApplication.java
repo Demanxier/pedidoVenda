12f6d5e73fce036baee6e51976512f1a5d9b35c8
Demanxier <demanxier.rodrigues@gmail.com>
2020-09-21 20:27:52 -0300

    Testando Rest

---
+++ src/main/java/br/com/pleco/DemanxierApplication.java
@@ -0,0 +1,13 @@
+package br.com.pleco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemanxierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemanxierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pleco/DemanxierApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pleco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemanxierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
